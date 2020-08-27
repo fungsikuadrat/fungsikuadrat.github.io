af8811812a76e17b15db711bf5f12e0fd7f47913
fungsikuadrat <xzxgaul@yahoo.com>
2020-08-27 14:04:26 +0800

    initial commit

--- script/pertanyaan2.js
+++ script/pertanyaan2.js
@@ -1,11 +1,12 @@
-let vals=[];
+let vals=["0","0","0","0","0","0","0","0","0","0"];
 valsa=0;
 let geo=document.getElementById("pp202");
 console.log("GEO "+geo);
+let titiks=document.getElementById("inputitik");
+let ketz=document.getElementById("ketz");
+let ketz2=document.getElementById("ketz2");
 
 function ttk1(){
-    console.log(geo);
-    console.log("OKE")
     valsa=0;
     let ttk=document.getElementById("ttk1").value;
     console.log(ttk)
@@ -18,12 +19,15 @@ function ttk1(){
     }
     else{
         document.getElementById("ttk1").style.borderColor="red";
+        console.log("titks "+titiks);
     }
     for(let i=0; i<vals.length; i++){
         valsa = valsa+vals[i];
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz2.classList.toggle("hilangs");
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
     }
     
@@ -44,7 +48,9 @@ function ttk2(){
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 
 }
@@ -63,7 +69,9 @@ function ttk3(){
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 }
 function ttk4(){
@@ -81,7 +89,9 @@ function ttk4(){
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 }
 function ttk5(){
@@ -102,7 +112,9 @@ function ttk5(){
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 }
 
@@ -112,20 +124,28 @@ function coord1(){
     let coord=document.getElementById("coord1").value;
     if(coord==""){
         document.getElementById("coord1").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     else if(coord=="-1,0"||coord=="-1.0"){
         document.getElementById("coord1").style.borderColor="#006A4E";
+        titiks.classList.toggle("hilangs");
         vals[5]= 1;
     }
     else{
         document.getElementById("coord1").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     for(let i=0; i<vals.length; i++){
         valsa = valsa+vals[i];
         console.log("VALSa "+valsa);
     }
+    // if(vals[5]==1||vals[0]==0){
+    //     titiks.classList.toggle("hilangs");
+    // }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 }
 function coord2(){
@@ -133,20 +153,25 @@ function coord2(){
     let coord=document.getElementById("coord2").value;
     if(coord==""){
         document.getElementById("coord2").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     else if(coord=="0,9"||coord=="0.9"){
         document.getElementById("coord2").style.borderColor="#006A4E";
+        titiks.classList.toggle("hilangs");
         vals[6]= 1;
     }
     else{
         document.getElementById("coord2").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     for(let i=0; i<vals.length; i++){
         valsa = valsa+vals[i];
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz.classList2.toggle("hilangs");
     }
 }
 function coord3(){
@@ -154,20 +179,25 @@ function coord3(){
     let coord=document.getElementById("coord3").value;
     if(coord==""){
         document.getElementById("coord3").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     else if(coord=="1,12"||coord=="1.12"){
         document.getElementById("coord3").style.borderColor="#006A4E";
+        titiks.classList.toggle("hilangs");
         vals[7]= 1;
     }
     else{
         document.getElementById("coord3").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     for(let i=0; i<vals.length; i++){
         valsa = valsa+vals[i];
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 }
 function coord4(){
@@ -175,20 +205,25 @@ function coord4(){
     let coord=document.getElementById("coord4").value;
     if(coord==""){
         document.getElementById("coord4").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     else if(coord=="2,9"||coord=="2.9"){
         document.getElementById("coord4").style.borderColor="#006A4E";
+        titiks.classList.toggle("hilangs");
         vals[8]= 1;
     }
     else{
         document.getElementById("coord4").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     for(let i=0; i<vals.length; i++){
         valsa = valsa+vals[i];
         console.log("VALSa "+valsa);
     }
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 }
 function coord5(){
@@ -196,20 +231,31 @@ function coord5(){
     let coord=document.getElementById("coord5").value;
     if(coord==""){
         document.getElementById("coord5").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     else if(coord=="3,0"||coord=="3.0"){
         document.getElementById("coord5").style.borderColor="#006A4E";
+        titiks.classList.toggle("hilangs");
         vals[9]= 1;
     }
     else{
         document.getElementById("coord5").style.borderColor="red";
+        titiks.classList.remove("hilangs");
     }
     for(let i=0; i<vals.length; i++){
         valsa = valsa+vals[i];
         console.log("VALSa "+valsa);
     }
+    
     if(valsa==10){
+        ketz.classList.toggle("hilangs");
         geo.classList.toggle("hilangs");
+        ketz2.classList.toggle("hilangs");
     }
 }
 
+let p2kal1=document.getElementById("p2kal1");
+let kalkul1=document.getElementById("kalkul1");
+p2kal1.addEventListener('click', function(){
+    kalkul1.classList.toggle("hilangkal");
+});
