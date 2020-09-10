236c20bd1bf3491cc7a08d0ad3f2cb8210c8d1cd
fungsikuadrat <xzxgaul@yahoo.com>
2020-09-10 23:51:03 +0800

    initial commit

--- script/evaluasi.js
+++ script/evaluasi.js
@@ -80,7 +80,7 @@ dat.onreadystatechange = function () {
         let dataku = JSON.parse(this.responseText);
 
         //random urutan
-        for (var x = 0; x < 100; x++) {
+        for (var x = 0; x < 500; x++) {
             // acak angka dengan batas length.data dan minimum 0
             var xx = Math.floor(Math.random() * dataku.length) + 0;
             cek.push(xx);
